d56bc7ebc90c2b8749f881e13b56b8e5724c9620
junyoung <jadw9715@gmail.com>
2023-04-28 00:21:11 +0900

    add: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,41 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+// Font files can be colocated inside of `app`
+const pretendard = localFont({
+  src: [
+    {
+      path: "./assets/fonts/pretendard/Pretendard-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./assets/fonts/pretendard/Pretendard-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./assets/fonts/pretendard/Pretendard-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./assets/fonts/pretendard/Pretendard-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./assets/fonts/pretendard/Pretendard-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./assets/fonts/pretendard/Pretendard-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata = {
   title: "GIVEN.LOG",
@@ -15,7 +49,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }

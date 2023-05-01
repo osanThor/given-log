b78389c7f2194d34c3d5eb168caab250df67a344
junyoung <jadw9715@gmail.com>
2023-05-02 01:58:44 +0900

    style. editor

--- src/components/admin/boards/Editor/index.tsx
+++ src/components/admin/boards/Editor/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import styles from "./editor.module.css";
 import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import "react-quill/dist/quill.bubble.css";
 
 const modules = {
   toolbar: [
@@ -38,6 +38,7 @@ export default function Editor() {
   return (
     <div id="editor" className={styles.editorBlock}>
       <ReactQuill
+        theme="bubble"
         className={styles.quill}
         modules={modules}
         scrollingContainer={"#editor"}

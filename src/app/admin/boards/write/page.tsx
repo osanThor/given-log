bae42d662bf98d6d85bac4f1358596d4fddbcbdf
junyoung <jadw9715@gmail.com>
2023-05-02 00:37:51 +0900

    feat.tag add, delete:

--- src/app/admin/boards/write/page.tsx
+++ src/app/admin/boards/write/page.tsx
@@ -1,9 +1,105 @@
+"use client";
+
+import { useState } from "react";
+import uuid from "react-uuid";
+import { AiFillCloseCircle } from "react-icons/ai";
+
 export default function BoardWritePage() {
+  const [tag, setTag] = useState<string>("");
+  const [tags, setTags] = useState<Array<string>>([]);
+  const handleChangeTag = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setTag(value);
+  };
+  const handleAddTag = () => {
+    if (tags.includes(tag)) {
+      return alert("존재하는 tag");
+    }
+    setTags((prev) => [...prev, tag]);
+    setTag("");
+  };
+  const handleDeleteTag = (str: string) => {
+    setTags(tags.filter((el) => el !== str));
+  };
+
   return (
     <div className="w-full">
-      {/** TODO: FEATURED, TITLE, THUMBNAIL, CONTANT, TAGS, POST  */}
-      <div className="px-2 py-4 font-bold text-gray-800 border-b border-gray-300">
+      {/** TODO: FEATURED, TITLE,SUBTITLE,TAGS, THUMBNAIL, CONTANT,  POST  */}
+      <div className="px-2 py-4 mb-4 font-bold text-gray-800 border-b border-gray-300">
         글쓰기
+      </div>
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-2 ">
+          <label className="min-w-[120px]">Featured</label>
+          <input type="checkbox" name="featured" />
+        </div>
+        <div className="flex gap-2 ">
+          <label className="min-w-[120px]">Title</label>
+          <input
+            type="text"
+            name="title"
+            autoComplete="title"
+            className="w-full h-10 px-2 border border-gray-300 rounded focus:border-blue-500"
+          />
+        </div>
+        <div className="flex gap-2 ">
+          <label className="min-w-[120px]">Sub Title</label>
+          <input
+            type="text"
+            name="subTitle"
+            autoComplete="subTitle"
+            className="w-full h-10 px-2 border border-gray-300 rounded focus:border-blue-500"
+          />
+        </div>
+        <div className="flex gap-2 ">
+          <label className="min-w-[120px]">Tags</label>
+          <div className="flex flex-col w-full">
+            <div className="flex w-full gap-1">
+              <input
+                type="text"
+                name="tags"
+                autoComplete="tags"
+                value={tag}
+                onChange={handleChangeTag}
+                className="w-full h-10 px-2 border border-gray-300 rounded focus:border-blue-500"
+              />
+              <button
+                onClick={handleAddTag}
+                className="px-2 py-1 bg-blue-500 min-w-[80px] rounded text-white text-xs font-bold "
+              >
+                추가
+              </button>
+            </div>
+            {tags.length > 0 && (
+              <div className="flex flex-wrap w-full gap-2 py-4">
+                {tags.map((el) => (
+                  <div
+                    key={uuid()}
+                    className="flex items-center gap-1 px-2 py-1 text-gray-700 border border-gray-700 rounded"
+                  >
+                    {`#${el}`}
+                    <button onClick={() => handleDeleteTag(el)}>
+                      <AiFillCloseCircle />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="flex gap-2 ">
+          <label className="min-w-[120px]">Thumbnail</label>
+          <input type="file" name="thumbnail" className="h-10" />
+        </div>
+        <div className="flex gap-2 ">
+          <label className="min-w-[120px]">Contant</label>
+          <input
+            type="text"
+            name="subTitle"
+            autoComplete="subTitle"
+            className="w-full h-10 px-2 border border-gray-300 rounded focus:border-blue-500"
+          />
+        </div>
       </div>
     </div>
   );

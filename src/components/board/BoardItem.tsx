175a42203fd0ae2f7a79a7ad291eda3355c84100
junyoung <jadw9715@gmail.com>
2023-05-03 23:33:36 +0900

    add. subtitle

--- src/components/board/BoardItem.tsx
+++ src/components/board/BoardItem.tsx
@@ -8,7 +8,10 @@ export default function BoardItem() {
         <Image src={NoImage} alt="no-image" className="h-40" />
         <div className="px-2 py-4">
           <span className="mb-2 text-xs font-bold text-green-500">DEV</span>
-          <h1 className="w-full mb-2 font-bold truncate">title</h1>
+          <h1 className="w-full mb-1 font-bold truncate">title</h1>
+          <h1 className="w-full mb-2 text-sm text-gray-400 truncate">
+            subTitle
+          </h1>
           <div className="flex justify-between">
             <span className="text-xs text-gray-500">2023.12.23</span>
             {/* 후에 댓글, 좋아요 count 추가   */}

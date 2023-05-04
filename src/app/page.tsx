6dc474c1a161dec8345b62f80e08cbf02da6f21a
given0 <88120986+osanThor@users.noreply.github.com>
2023-05-05 04:34:31 +0900

    Dev.ljy (#5)
    * add: font
    
    * feat. header reponsive
    
    * add. container components
    
    * add. title component
    
    * feat. list and item layout
    
    * style. board list item
    
    * feat. swip list component
    
    * add. Footer component
    
    * feat. category pages
    
    * feat. category layoout and fix.router
    
    * add. about, contact page
    
    * add. admin login page
    
    * setting. firebase setting
    
    * fix. .gitignore
    
    * feat. login func
    
    * add. admin boards, weite page
    
    * feat. admin layout
    
    * feat.tag add, delete:
    
    * add. editor component
    
    * style. editor
    
    * style. editor
    
    * feat. post board api setting
    
    * feat. POST board api
    
    * feat. cloudinary
    
    * feat. image handler
    
    * feat. post event handler
    
    * add. subtitle
    
    * feat. skeleton item
    
    * feat. board item 정보 표시
    
    * feat. get latest list API
    
    * fix. build error
    
    * fix. firestore timestemp to string
    
    * fix. build error
    
    * feix. build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
 import List from "@/components/board/List";
 import SlideList from "@/components/board/SlideList";
 import Title from "@/components/common/Title";
+import { InGetLogProps } from "@/interfaces/in_Boards";
 import { getMainList } from "@/services/get_list";
 
-const HomePage = async () => {
-  const result = await getMainList();
+interface Props {
+  latestList: Array<InGetLogProps>;
+  featuredist: Array<InGetLogProps>;
+}
 
+async function getData() {
+  const res = await fetch("http://localhost:300/api/boards/getList/latest", {
+    next: { revalidate: 10 },
+  });
+  const data = res.json();
+  return data;
+  // const result = await getMainList();
+  // const latestList = await result[0].data;
+  // const featuredist = await result[1].data;
+  // return { props: { latestList, featuredist } };
+}
+
+const HomePage = async () => {
+  // const data = await getData();
   return (
     <>
       <Title title="📑 Latest Logs" />
-      <List list={result[0].data} />
+      {/* <List list={data} /> */}
       <Title title="⭐️ Featured Logs" />
-      <SlideList list={result[1].data} />
+      {/* <SlideList list={data} /> */}
     </>
   );
 };
 
 export default HomePage;
-
--- src/components/board/List.tsx
+++ src/components/board/List.tsx
@@ -5,9 +5,7 @@ import { InGetLogProps } from "@/interfaces/in_Boards";
 export default function List({ list }: { list: Array<InGetLogProps> }) {
   return (
     <div className="grid w-full grid-cols-1 gap-8 py-4 mb-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {list.map((el) => (
-        <BoardItem key={uuid()} item={el} />
-      ))}
+      {list && list.map((el) => <BoardItem key={uuid()} item={el} />)}
     </div>
   );
 }

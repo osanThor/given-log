327c8218be991f6199ff50812042ee3c565a9bec
junyoung <jadw9715@gmail.com>
2023-05-04 23:18:09 +0900

    feat. get latest list API

--- src/models/boards.model.ts
+++ src/models/boards.model.ts
@@ -89,11 +89,34 @@ async function getFeaturedList() {
   return devLogs.concat(lifeLogs).slice(0, 8);
 }
 
+async function getLatestList() {
+  const logs: any[] = [];
+
+  const blogRef = Firestore.collection(BLOG_COL);
+  const docsQuerySnapshot = await blogRef
+    .where("category", "in", [DEV_DOC, LIFE_DOC])
+    .get();
+  // 각 문서에서 "logs" 하위 컬렉션에서 createAt 기준으로 최신글 8개 가져오기
+  for (const doc of docsQuerySnapshot.docs) {
+    const logsRef = blogRef.doc(doc.id).collection(LOGS_COL);
+    const logsQuerySnapshot = await logsRef
+      .orderBy("createAt", "desc")
+      .limit(8)
+      .get();
+    logsQuerySnapshot.forEach((logDoc) => {
+      const { title, subTitle, category, createAt, thumbnail } = logDoc.data();
+      logs.push({ title, subTitle, category, createAt, thumbnail });
+    });
+  }
+  // 최신글 8개만 반환
+  return logs.sort((a, b) => b.createAt - a.createAt).slice(0, 8);
+}
+
 async function getList({ category, page = 1, size = 8 }: InGetListProps) {
   const blogRef = Firestore.collection(BLOG_COL);
   const devRef = blogRef.doc(DEV_DOC).collection(LOGS_COL);
   const lifeRef = blogRef.doc(LIFE_DOC).collection(LOGS_COL);
 }
 
-const BoardsModel = { post, getFeaturedList };
+const BoardsModel = { post, getFeaturedList, getLatestList };
 export default BoardsModel;

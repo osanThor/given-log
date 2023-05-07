6992f8a215312cd3971f61661d8e075292c40041
junyoung <jadw9715@gmail.com>
2023-05-08 03:41:57 +0900

    style. loading page

--- src/app/category/loading.tsx
+++ src/app/category/loading.tsx
@@ -7,11 +7,6 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 export default function MainPageLoading() {
   return (
     <>
-      <div className="flex flex-col items-center w-full gap-1 py-5 border-b border-gray-200">
-        <Skeleton width={100} height={100} borderRadius={100} />
-        <Skeleton width={120} />
-        <Skeleton width={70} />
-      </div>
       <div className="py-2 border-b border-gray-300">
         <Skeleton height={30} />
       </div>

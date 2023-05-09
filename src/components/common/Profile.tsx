6a3b504e42a2d231d5a20b1303f2cbd6089bbad8
junyoung <jadw9715@gmail.com>
2023-05-09 23:40:50 +0900

    add. about me lottie image

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -5,14 +5,14 @@ export default function Profile() {
   return (
     <div className="flex flex-col items-center w-full gap-1 py-5 border-b border-gray-200">
       <div className="relative w-[100px] h-[100px] overflow-hidden border border-gray-300 rounded-full">
-        <Img src="/assets/images/profile.jpeg" alt="Rounded avatar" />
+        <Img src="/assets/images/profile.png" alt="Rounded avatar" />
       </div>
       <p>Hi! My Name is JunYoung</p>
       <Link
         href="/contact"
         className="px-2 py-1 text-sm text-white bg-blue-500 rounded-lg hover:bg-blue-300"
       >
-        GIVEN
+        Contact
       </Link>
     </div>
   );

805ea5c9ad7e39a799b94a1d9de92a0c1ae4e101
given0 <88120986+osanThor@users.noreply.github.com>
2023-05-25 23:34:37 +0900

    Dev.ljy (#21)
    * add: font
    
    * feat. header reponsive
    
    * add. container components
    
    * add. title component
    
    * feat. list and item layout
    
    * style. board list item
    
    * feat. swip list component
    
    * add. Footer component
    
    * feat. category pages
    
    * feat. category layoout and fix.router
    
    * add. about, contact page
    
    * add. admin login page
    
    * setting. firebase setting
    
    * fix. .gitignore
    
    * feat. login func
    
    * add. admin boards, weite page
    
    * feat. admin layout
    
    * feat.tag add, delete:
    
    * add. editor component
    
    * style. editor
    
    * style. editor
    
    * feat. post board api setting
    
    * feat. POST board api
    
    * feat. cloudinary
    
    * feat. image handler
    
    * feat. post event handler
    
    * add. subtitle
    
    * feat. skeleton item
    
    * feat. board item 정보 표시
    
    * feat. get latest list API
    
    * fix. build error
    
    * fix. firestore timestemp to string
    
    * fix. build error
    
    * feix. build error
    
    * add. board page
    
    * fix. build error done
    
    * feat. loading skeleton image
    
    * feat. get list API category and tag
    
    * feat. category page
    
    * fix. build error
    
    * fix. build error2
    
    * fix. build error3
    
    * feat. return allTags
    
    * feat. tag box
    
    * feat. get board item
    
    * feat. board page
    
    * fix. mobile header link
    
    * style. board loading
    
    * style. board page custom
    
    * fix. tag box
    
    * feat. profile component
    
    * style. loading page
    
    * add. about me lottie image
    
    * add. contact lottie image
    
    * add. node emailer 추가
    
    * feat. send email API
    
    * feat. node mailer
    
    * feat. contact form
    
    * feat. post contact
    
    * fix. main page SSR
    
    * feat. editor viewer
    
    * setting. react-query setting
    
    * fix. serverside prerendering error
    
    * refactor. 불필요한 로딩 삭제
    
    * fix. header menu active option
    
    * fix. Minified React error #425
    
    * fix. loading 속도 개선
    
    * fix. 속도 개선2 react-query 적용
    
    * add. list loading
    
    * refactor. getAllTags API 분리
    
    * fix. tag 박스 깜빡임 오류
    
    * Refactor. 불필요 주석 삭제
    
    * fix. index 변경
    
    * fix. 최적화

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const withPlugins = require("next-compose-plugins");
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -13,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([withBundleAnalyzer], nextConfig);

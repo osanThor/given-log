ce0143a6c061ce779a6dc398806eb9c512554579
junyoung <jadw9715@gmail.com>
2024-03-12 21:58:28 +0900

    Style. container padding 조정

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col items-center w-full h-full p-4 px-6 ">
+    <div className="flex flex-col items-center w-full h-full p-4 px-2 ">
       <div className="container min-h-[700px]">{children}</div>
     </div>
   );

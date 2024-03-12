5ea8dcb2d011f5435a163190c904561496c7b0f6
junyoung <jadw9715@gmail.com>
2024-03-12 22:01:51 +0900

    Style. Footer padding x 조정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="m-4 bg-white rounded-lg shadow dark:bg-gray-800">
+    <footer className="my-4 mx-2 md:m-4 bg-white rounded-lg shadow dark:bg-gray-800">
       <div className="container p-4 mx-auto md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023

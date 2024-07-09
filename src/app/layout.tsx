e8f2b623cd9adea11921aaa1c49504e0c54c4273
given <88120986+osanThor@users.noreply.github.com>
2024-07-09 12:38:57 +0900

    Dev.ljy (#43)
    * Style. container padding 조정
    
    * Style. Footer padding x 조정
    
    * Style. header 변경
    
    * Refactor. 오류 수정 및 레이아웃 정리
    
    * Fix. nodemailer ERROR 수정
    
    * Fix. Response 변경
    
    * Fix, Build 에러
    
    * Style. Board Item Image
    
    * Style. board Item height
    
    * Style. 파비콘 변경 및 스타일 수정
    
    * Add. robots.txt  추가
    
    * Meta. Meta icons 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,10 @@ import Footer from "@/components/common/Footer";
 import { AuthProvider } from "@/contexts/auth.context";
 import ReactQueryProvider from "@/contexts/ReactQuery";
 
-// Font files can be colocated inside of `app`
+const BASE_URL = new URL(
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+);
+
 const pretendard = localFont({
   src: [
     {
@@ -45,9 +48,83 @@ const pretendard = localFont({
 
 export const metadata: Metadata = {
   title: { default: "GIVEN's LOG", template: "GIVEN's LOG | %s" },
-  description: "Front End Developer Blog",
+  description: "Front End Developer Given's Blog",
+  metadataBase: BASE_URL,
+  alternates: {
+    canonical: "/",
+  },
   icons: {
-    icon: "/favicon.ico",
+    icon: { rel: "icon", url: "/favicon.ico", sizes: "48x48" },
+    apple: [
+      { rel: "", url: `${BASE_URL}/assets/images/apple-icon.png` },
+      {
+        url: `${BASE_URL}/assets/images/apple-icon-57x57.png`,
+        sizes: "57x57",
+        type: "image/png",
+      },
+      {
+        url: `${BASE_URL}/assets/images/apple-icon-60x60.png`,
+        sizes: "60x60",
+        type: "image/png",
+      },
+      {
+        url: `${BASE_URL}/assets/images/apple-icon-76x76.png`,
+        sizes: "76x76",
+        type: "image/png",
+      },
+      {
+        url: `${BASE_URL}/assets/images/apple-icon-114x114.png`,
+        sizes: "114x114",
+        type: "image/png",
+      },
+      {
+        url: `${BASE_URL}/assets/images/apple-icon-120x120.png`,
+        sizes: "120x120",
+        type: "image/png",
+      },
+      {
+        url: `${BASE_URL}/assets/images/apple-icon-144x144.png`,
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        url: `${BASE_URL}/assets/images/apple-icon-152x152.png`,
+        sizes: "152x152",
+        type: "image/png",
+      },
+      {
+        url: `${BASE_URL}/assets/images/apple-icon-180x180.png`,
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "apple-touch-icon-precomposed",
+        url: `${BASE_URL}/assets/images/apple-icon-precomposed.png`,
+        type: "image/png",
+      },
+    ],
+  },
+  openGraph: {
+    title: { default: "GIVEN's LOG", template: "GIVEN's LOG | %s" },
+    description: "Front End Developer Given's Blog",
+    url: BASE_URL,
+    siteName: "Given's log",
+    images: [
+      {
+        url: `${BASE_URL}/assets/images/op_image.png`,
+        width: 800,
+        height: 600,
+      },
+      {
+        url: `${BASE_URL}/assets/images/op_image.png`,
+        width: 1800,
+        height: 1600,
+        alt: "Given's log",
+      },
+    ],
+    type: "website",
   },
 };
 

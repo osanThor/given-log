e59fb7397dd91adbbc284090aa2a61bbadb92ea7
given <88120986+osanThor@users.noreply.github.com>
2024-07-25 22:31:45 +0900

    Dev.ljy (#46)
    * Style. container padding 조정
    
    * Style. Footer padding x 조정
    
    * Style. header 변경
    
    * Refactor. 오류 수정 및 레이아웃 정리
    
    * Fix. nodemailer ERROR 수정
    
    * Fix. Response 변경
    
    * Fix, Build 에러
    
    * Style. Board Item Image
    
    * Style. board Item height
    
    * Style. 파비콘 변경 및 스타일 수정
    
    * Add. robots.txt  추가
    
    * Meta. Meta icons 추가
    
    * Fix. Metadata URL 수정
    
    * Meta. naver site verification 추가
    
    * Change. portfolio URL 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
             <Link
-              href="https://my-works-app.vercel.app/"
+              href="https://www.given-log.com"
               target="_blank"
               className="mr-4 hover:underline md:mr-6"
             >
